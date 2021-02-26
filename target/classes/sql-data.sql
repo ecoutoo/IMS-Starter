0231baefc4a7ca4100f656f13726e1cfb06a8490
ecoutoo <viccontecouto@gmail.com>
2021-02-26 13:34:53 +0000

    Target added, fixed schema and data execution

---
+++ target/classes/sql-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `customers`(`first_name`, `surname`) VALUES ('LL', 'Stylish');
+INSERT INTO `items`(`item_name`, `item_price`) VALUES('cola', 1.50);
+INSERT INTO `orders`(`cust_id`) VALUES(1);
+INSERT INTO `orderlines`(`ord_id`, `item_id`, `quantity`) VALUES(1, 1, 2);
---
+++ target/test-classes/sql-schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS `orderlines`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE IF NOT EXISTS `customers` (
+    `cust_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`cust_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_price` DOUBLE DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `ord_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `cust_id` INT(11) NOT NULL,
+    PRIMARY KEY (`ord_id`),
+    FOREIGN KEY (`cust_id`) REFERENCES customers(`cust_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `orderlines` (
+    `line_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `ord_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    `quantity` INT(11) NOT NULL,
+    PRIMARY KEY (`line_id`),
+    FOREIGN KEY (`ord_id`) REFERENCES orders(`ord_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`item_id`) REFERENCES items(`item_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
